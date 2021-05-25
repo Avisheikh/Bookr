178ae48a5e681c0f3a47718b7f9922f78b4ac024
Abishek Bhandari <abishekbhandari33@gmail.com>
2021-05-25 17:59:28 +0545

    Finished chapter one activity

--- bookr/reviews/views.py
+++ bookr/reviews/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    return render(request, "base.html")
+
+
+def search(request):
+
+    searchResults = request.GET.get("searchResults") or "Not Found"
+    
+    return render(request, "search.html",{"sec": searchResults})
