da0354ebc76f52b76046022f0997c760efcdd1b5
Abishek Bhandari <abishekbhandari33@gmail.com>
2021-06-03 21:14:19 +0545

    Finished django custome admin funcationlity

--- bookr/reviews/admin.py
+++ bookr/reviews/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from reviews.models import  Contributor, Book, BookContributor, Review
+
+
+def initialled_name(obj):
+    initials = ''.join([name[0] for name in obj.first_names.split(' ')])
+    return "{}, {}".format(obj.last_names, initials)
+
+class ContributorAdmin(admin.ModelAdmin):
+    list_display = (initialled_name,)    
+
+class BookAdmin(admin.ModelAdmin):
+
+    date_hierarchy = 'publication_date'
+    list_display = ('title','isbn')
+    list_filter = ('publisher', 'publication_date')
+    search_fields = ('title', 'isbn__exact', 'publisher__name')
+
+    def isbn13(self, obj):
+
+        return "{} - {} - {} - {} - {}".format(obj.isbn[0:3], obj.isbn[3:4], obj.isbn[4:6], obj.isbn[6:12], obj.isbn[12:13])
+
+
 
 # Register your models here.
+
+admin.site.register(Contributor)
+admin.site.register(Book, BookAdmin)
+admin.site.register(BookContributor)
+admin.site.register(Review)
+
+
--- bookr/reviews/models.py
+++ bookr/reviews/models.py
@@ -1,3 +1,4 @@
+
 from django.db import models
 from django.contrib import auth
 
@@ -25,6 +26,11 @@ class Book(models.Model):
         return self.title
 
 
+    def isbn13(self, obj):
+
+        return "{} - {} - {} - {} - {}".format(obj.isbn[0:3], obj.isbn[3:4], obj.isbn[4:6], obj.isbn[6:12], obj.isbn[12:13])
+
+
 
 class Contributor(models.Model):
     """
@@ -35,6 +41,7 @@ class Contributor(models.Model):
     first_names = models.CharField(max_length=50, help_text="The contributor's first name or names.")
     last_names = models.CharField(max_length=50, help_text="The contributor's last name or names.")
     email = models.EmailField(help_text="The contact email for the contributor.")
+    
 
     def __str__(self):
         return self.first_names
