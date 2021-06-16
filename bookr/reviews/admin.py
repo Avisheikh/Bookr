4ffaabdc1a26f13e6561eef8fa09713d679c8687
Abishek Bhandari <abishekbhandari33@gmail.com>
2021-06-16 20:46:35 +0545

    form validation finished

--- bookr/reviews/admin.py
+++ bookr/reviews/admin.py
@@ -1,32 +1,28 @@
 from django.contrib import admin
-from reviews.models import  Contributor, Book, BookContributor, Review
+from reviews.models import (Publisher, Contributor, Book,
+        BookContributor, Review)
 
+
+class BookAdmin(admin.ModelAdmin):
+    date_hierarchy = 'publication_date'
+    list_display = ('title', 'isbn')
+    list_filter = ('publisher', 'publication_date')
 
 def initialled_name(obj):
+    """ obj.first_names='Jerome David', obj.last_names='Salinger'
+        => 'Salinger, JD' """
     initials = ''.join([name[0] for name in obj.first_names.split(' ')])
     return "{}, {}".format(obj.last_names, initials)
 
 class ContributorAdmin(admin.ModelAdmin):
-    list_display = (initialled_name,)    
-
-class BookAdmin(admin.ModelAdmin):
-
-    date_hierarchy = 'publication_date'
-    list_display = ('title','isbn')
-    list_filter = ('publisher', 'publication_date')
-    search_fields = ('title', 'isbn__exact', 'publisher__name')
-
-    def isbn13(self, obj):
-
-        return "{} - {} - {} - {} - {}".format(obj.isbn[0:3], obj.isbn[3:4], obj.isbn[4:6], obj.isbn[6:12], obj.isbn[12:13])
-
+    list_display = ('last_names', 'first_names')
+    list_filter = ('last_names',)
+    search_fields = ('last_names__startswith', 'first_names')
 
 
 # Register your models here.
-
-admin.site.register(Contributor)
+admin.site.register(Publisher)
+admin.site.register(Contributor, ContributorAdmin)
 admin.site.register(Book, BookAdmin)
 admin.site.register(BookContributor)
 admin.site.register(Review)
-
-
--- bookr/reviews/models.py
+++ bookr/reviews/models.py
@@ -1,13 +1,13 @@
-
-from django.db import models
 from django.contrib import auth
+from django.db import models
+
 
 class Publisher(models.Model):
     """A company that publishes books."""
-    name = models.CharField(max_length = 50, help_text = "The name of the publisher.")
-    website = models.URLField(help_text="The Publisher website,")
-    email = models.EmailField(help_text = "The publisher's email address.")
-
+    name = models.CharField(max_length=50,
+                            help_text="The name of the Publisher.")
+    website = models.URLField(help_text="The Publisher's website.")
+    email = models.EmailField(help_text="The Publisher's email address.")
 
     def __str__(self):
         return self.name
@@ -15,36 +15,45 @@ class Publisher(models.Model):
 
 class Book(models.Model):
     """A published book."""
-
-    contributors = models.ManyToManyField('Contributor', through="BookContributor")
-    publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)
-    title = models.CharField(max_length=70, help_text="The title of the book.")
-    publication_date = models.DateField(verbose_name="Date the book wasw published.")
-    isbn = models.CharField(max_length=20, verbose_name="ISBN number of the book.")
+    title = models.CharField(max_length=70,
+                             help_text="The title of the book.")
+    publication_date = models.DateField(
+        verbose_name="Date the book was published.")
+    isbn = models.CharField(max_length=20,
+                            verbose_name="ISBN number of the book.")
+    publisher = models.ForeignKey(Publisher,
+                                  on_delete=models.CASCADE)
+    contributors = models.ManyToManyField('Contributor',
+                                          through="BookContributor")
 
     def __str__(self):
-        return self.title
+        return "{} ({})".format(self.title, self.isbn)
 
-
-    def isbn13(self, obj):
-
-        return "{} - {} - {} - {} - {}".format(obj.isbn[0:3], obj.isbn[3:4], obj.isbn[4:6], obj.isbn[6:12], obj.isbn[12:13])
-
+    def isbn13(self):
+        """ '9780316769174' => '978-0-31-676917-4' """
+        return "{}-{}-{}-{}-{}".format(self.isbn[0:3], self.isbn[3:4],
+                                       self.isbn[4:6], self.isbn[6:12],
+                                       self.isbn[12:13])
 
 
 class Contributor(models.Model):
-    """
-    A contributor to a book, e.g. author, editor, co-author.
-
-    """
-   
-    first_names = models.CharField(max_length=50, help_text="The contributor's first name or names.")
-    last_names = models.CharField(max_length=50, help_text="The contributor's last name or names.")
+    """A contributor to a Book, e.g. author, editor, co-author."""
+    first_names = models.CharField(max_length=50,
+                                   help_text="The contributor's first name or names.")
+    last_names = models.CharField(max_length=50,
+                                  help_text="The contributor's last name or names.")
     email = models.EmailField(help_text="The contact email for the contributor.")
-    
+
+    def initialled_name(self):
+        """ self.first_names='Jerome David', self.last_names='Salinger'
+            => 'Salinger, JD' """
+        initials = ''.join([name[0] for name
+                            in self.first_names.split(' ')])
+        return "{}, {}".format(self.last_names, initials)
 
     def __str__(self):
-        return self.first_names
+        return self.initialled_name()
+
 
 class BookContributor(models.Model):
     class ContributionRole(models.TextChoices):
@@ -54,12 +63,23 @@ class BookContributor(models.Model):
 
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
     contributor = models.ForeignKey(Contributor, on_delete=models.CASCADE)
-    role = models.CharField(verbose_name = "The role this contributor had in the book.", choices = ContributionRole.choices, max_length=20 )
+    role = models.CharField(verbose_name="The role this contributor had in the book.",
+                            choices=ContributionRole.choices, max_length=20)
+
+    def __str__(self):
+        return "{} {} {}".format(self.contributor.initialled_name(), self.role, self.book.isbn)
+    
 
 class Review(models.Model):
-    content = models.TextField(help_text="The review text.")
-    rating = models.IntegerField(help_text="The rating the reviewer has given.")
-    date_created = models.DateTimeField(auto_now_add=True, help_text="The date and time the review was created.")
-    date_edited = models.DateTimeField(null=True,help_text="The date and time the review was last edited.")
+    content = models.TextField(help_text="The Review text.")
+    rating = models.IntegerField(help_text="The the reviewer has given.")
+    date_created = models.DateTimeField(auto_now_add=True,
+                                        help_text="The date and time the review was created.")
+    date_edited = models.DateTimeField(null=True,
+                                       help_text="The date and time the review was last edited.")
     creator = models.ForeignKey(auth.get_user_model(), on_delete=models.CASCADE)
-    book = models.ForeignKey(Book, on_delete=models.CASCADE, help_text="The book that this review is for.")
+    book = models.ForeignKey(Book, on_delete=models.CASCADE,
+                             help_text="The Book that this review is for.")
+
+    def __str__(self):
+        return "{} - {}".format(self.creator.username, self.book.title)
